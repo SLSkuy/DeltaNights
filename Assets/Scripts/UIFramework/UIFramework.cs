9b994ee5925caa8a4b2d03d8e489f37c79b5d706
SL_Skuy <3063473749@qq.com>
2025-10-24 21:46:56 +0800

    完善UI框架，实现常用接口

--- Assets/Scripts/UIFramework/UIFramework.cs
+++ Assets/Scripts/UIFramework/UIFramework.cs
@@ -1,3 +1,5 @@
+using System;
+using UIFramework.Core;
 using UIFramework.Panel;
 using UIFramework.Window;
 using UnityEngine;
@@ -69,6 +71,7 @@ namespace UIFramework
                     Debug.LogError("[UIFramework] UI Frame lacks Window Layer]");
                 }
             }
+            _graphicRaycaster = GetComponent<GraphicRaycaster>();
         }
 
         private void BlockScreen()
@@ -79,6 +82,118 @@ namespace UIFramework
         private void UnblockScreen()
         {
             _graphicRaycaster.enabled = true;
+        }
+
+        public void ShowPanel(string id)
+        {
+            _panelLayer.ShowUIByID(id);
+        }
+
+        public void ShowPanel<T>(string id, T p) where T : IPanelProperties
+        {
+            _panelLayer.ShowUIByID(id, p);
+        }
+
+        public void HidePanel(string id)
+        {
+            _panelLayer.HideUIByID(id);
+        }
+
+        public void OpenWindow(string id)
+        {
+            _windowLayer.ShowUIByID(id);
+        }
+
+        public void OpenWindow<T>(string id, T p) where T : IWindowProperties
+        {
+            _windowLayer.ShowUIByID(id, p);
+        }
+
+        public void CloseWindow(string id)
+        {
+            _windowLayer.HideUIByID(id);
+        }
+
+        public void CloseCurrentWindow()
+        {
+            if(_windowLayer.CurrentWindow != null)CloseWindow(_windowLayer.CurrentWindow.UIControllerID);
+        }
+
+        /// <summary>
+        /// 根据传入的ID显示对应的UI界面，不分面板还是窗口
+        /// </summary>
+        /// <param name="id"></param>
+        public void ShowUI(string id)
+        {
+            Type type;
+            if (IsUIRegistered(id, out type)) {
+                if (type == typeof(IWindowController)) {
+                    OpenWindow(id);
+                }
+                else if (type == typeof(IPanelController)) {
+                    ShowPanel(id);
+                }
+            }
+            else {
+                Debug.LogError($"[UIFramework] Tried to open Screen id {id} but it's not registered as Window or Panel!");
+            }
+        }
+
+        public void RegisterUI(string id, IUIController uiController, Transform uiTransform)
+        {
+            switch (uiController)
+            {
+                case IWindowController window when uiTransform:
+                    _windowLayer.RegisterUIController(id, window);
+                    _windowLayer.ReParentUI(window, uiTransform);
+                    break;
+                case IPanelController panel when uiTransform:
+                    _panelLayer.RegisterUIController(id, panel);
+                    _panelLayer.ReParentUI(panel, uiTransform);
+                    break;
+                default:
+                    Debug.LogError("[UIFramework] Transform is null or Unknown uiController");
+                    break;
+            }
+        }
+
+        public void UnregisterUI(string id, IUIController uiController)
+        {
+            switch (uiController)
+            {
+                case IWindowController window:
+                    _windowLayer.UnregisterUIController(id, window);
+                    break;
+                case IPanelController panel:
+                    _panelLayer.UnregisterUIController(id, panel);
+                    break;
+                default:
+                    Debug.LogError($"[UIFramework] {id} is not registered");
+                    break;
+            }
+        }
+
+        public void HideAll(bool animate = true)
+        {
+            _panelLayer.HideAllUI(animate);
+            _windowLayer.HideAllUI(animate);
+        }
+
+        public bool IsUIRegistered(string id, out Type type)
+        {
+            if (_windowLayer.IsRegistered(id))
+            {
+                type = typeof(IWindowController);
+                return true;
+            }
+            if (_panelLayer.IsRegistered(id))
+            {
+                type = typeof(IPanelController);
+                return true;
+            }
+
+            type = null;
+            return false;
         }
     }
 }
