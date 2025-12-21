92a466758589c5bb51702feaeb2d2cd7afb4e908
SL_Skuy <3063473749@qq.com>
2025-12-21 21:03:54 +0800

    完善技能冷却

--- Assets/Scripts/PlayerControl/PlayerSkillController.cs
+++ Assets/Scripts/PlayerControl/PlayerSkillController.cs
@@ -1,8 +1,19 @@
 using System.Collections.Generic;
 using SkillSystem;
+using UnityEngine;
 
 namespace PlayerControl
 {
+    /// <summary>
+    /// 单个技能实例
+    /// </summary>
+    public class SkillEntry
+    {
+        public ISkill Skill;
+        public float SkillCountDown;
+        public int CurrentCharges;
+    }
+    
     /// <summary>
     /// 玩家技能控制器
     /// </summary>
@@ -10,7 +21,7 @@ namespace PlayerControl
     {
         private readonly PlayerAttackController _attackController;
         
-        private readonly Dictionary<SkillType, ISkill> _skills = new Dictionary<SkillType, ISkill>();
+        private readonly Dictionary<SkillType, SkillEntry> _skillEntries = new();
         private ISkill _currentSkill;   // 当前选中技能
         
         private bool _isFinished = true;    // 当前技能是否已释放完毕
@@ -32,12 +43,26 @@ namespace PlayerControl
             _attackController.OnSkillReleased -= SkillUsed;
 
             // 取消事件订阅
-            foreach (var skills in _skills.Values)
+            foreach (var skillEntry in _skillEntries.Values)
             {
-                skills.OnFinished -= SkillFinished;
+                skillEntry.Skill.OnFinished -= SkillFinished;
             }
         }
-
+        
+        /// <summary>
+        /// 逻辑更新
+        /// </summary>
+        public void Tick(float deltaTime)
+        {
+            UpdateCountDown(deltaTime);
+            
+            // 当前已选中技能，且技能并没有释放完成，且如果为瞬发技能并没有释放技能按键
+            if (_currentSkill != null && !_isFinished && !_instantSkillHolding)
+            {
+                _currentSkill.SkillUpdate(deltaTime);
+            }
+        }
+        
         /// <summary>
         /// 注册技能信息
         /// </summary>
@@ -45,21 +70,43 @@ namespace PlayerControl
         /// <param name="skill">技能实例</param>
         public void RegisterSkill(SkillType skillType, ISkill skill)
         {
-            if (_skills.TryAdd(skillType, skill))
+            SkillEntry entry = new SkillEntry()
             {
-                skill.OnFinished += SkillFinished;
+                Skill = skill,
+                CurrentCharges = 1,
+                SkillCountDown = skill.Cooldown
+            };
+
+            if (_skillEntries.TryAdd(skillType, entry))
+            {
+                entry.Skill.OnFinished += SkillFinished;
             }
         }
 
         /// <summary>
-        /// 逻辑更新
+        /// 更新冷却时间
         /// </summary>
-        public void Tick(float deltaTime)
+        private void UpdateCountDown(float deltaTime)
         {
-            // 当前已选中技能，且技能并没有释放完成，且如果为瞬发技能并没有释放技能按键
-            if (_currentSkill != null && !_isFinished && !_instantSkillHolding)
+            foreach (var entry in _skillEntries.Values)
             {
-                _currentSkill.SkillUpdate(deltaTime);
+                // 无法恢复的技能，取消冷却逻辑计算
+                if (entry.Skill.LimitedUseSkill) continue;
+                
+                // 技能存储已达上限，不进行冷却计算
+                if (entry.CurrentCharges == entry.Skill.MaxCharges) return;
+                
+                if (entry.SkillCountDown > 0)
+                {
+                    entry.SkillCountDown -= deltaTime;
+                }
+                else
+                {
+                    // 恢复技能使用次数，重置冷却时间
+                    entry.CurrentCharges++;
+                    entry.SkillCountDown = entry.Skill.Cooldown;
+                    Debug.Log("技能恢复");
+                }
             }
         }
 
@@ -68,11 +115,13 @@ namespace PlayerControl
         /// </summary>
         private void InstantSkillProcess(SkillType skillType)
         {
-            _currentSkill = _skills[skillType];
+            // 技能当前无法使用
+            if(_skillEntries[skillType].CurrentCharges <= 0) return;
+            
+            _currentSkill = _skillEntries[skillType].Skill;
 
             if (_currentSkill.IsInstant)
             {
-                _isFinished = true;
                 _instantSkillHolding = true;
                 SkillArmed(skillType);
             }
@@ -105,7 +154,10 @@ namespace PlayerControl
 
         private void SkillArmed(SkillType skillType)
         {
+            if (!_isFinished) return;
+
             _isFinished = false;
+            _currentSkill = _skillEntries[skillType].Skill;
             _currentSkill.SkillArmed();
         }
 
@@ -119,12 +171,17 @@ namespace PlayerControl
 
         private void SkillUsed(SkillType skillType)
         {
-            if (_instantSkillHolding)
-            {
-                _instantSkillHolding = false;
-                _currentSkill.SkillUsed();
-            }
+            if (!_instantSkillHolding) return;
+
+            var entry = _skillEntries[skillType];
+            if (entry.CurrentCharges <= 0) return;
+
+            entry.CurrentCharges--;
+            _instantSkillHolding = false;
+
+            _currentSkill?.SkillUsed();
         }
+
         
         #endregion
     }
