745daf5c333ad8464b6c7e3e4e2ea0896c5b67bb
2023051604032WangXinKai <1975935954@qq.com>
2025-12-23 23:29:14 +0800

    测试UI框架传参 提交名更改，原提交名为 星焰

--- Assets/Scripts/SceneUI/StartSceneUI/StartMainPanel.cs
+++ Assets/Scripts/SceneUI/StartSceneUI/StartMainPanel.cs
@@ -1,7 +1,19 @@
-﻿using EventProcess;
+﻿/* ------------------------------------------------------------
+ *  Author:  2023051604032 WangXinKai
+ *  Date:  2025.12.23
+ *  LastUpdate:  2025.12.23
+ * 
+ *  功能简述：
+ *  游戏开始界面的主界面
+ * 
+ * ------------------------------------------------------------ */
+
+using EventProcess;
+using TMPro;
 using UIFramework;
 using UIFramework.Panel;
-using UnityEngine.UI;
+using UnityEngine;
+
 
 
 namespace SceneUI.StartSceneUI
@@ -10,9 +22,29 @@ namespace SceneUI.StartSceneUI
     
     public class StartMainPanel : PanelController
     {
+        [SerializeField]private TMP_Text text;
         public void UI_OnStartButtonPressDown()
         {
             Signals.Get<StartPressDownSignal>().Dispatch();
+        }
+        
+        protected override void SetProperties(PanelProperties props)
+        {
+            if (props is StartMainPanelProperties startMainPanelProperties)
+            {
+                text.text = startMainPanelProperties.Content;
+            }
+            base.SetProperties(props);
+        }
+    }
+    
+    public class StartMainPanelProperties : PanelProperties
+    {
+        public string Content;
+        
+        public StartMainPanelProperties(PanelPriority priority,string content) : base(priority)
+        {
+            this.Content = content;
         }
     }
 }
--- Assets/Scripts/SceneUI/StartSceneUI/StartUIManager.cs
+++ Assets/Scripts/SceneUI/StartSceneUI/StartUIManager.cs
@@ -1,5 +1,16 @@
-﻿using EventProcess;
+﻿/* ------------------------------------------------------------
+ *  Author:  2023051604032 WangXinKai
+ *  Date:  2025.12.23
+ *  LastUpdate:  2025.12.23
+ * 
+ *  功能简述：
+ *  游戏开始界面的UI管理器
+ * 
+ * ------------------------------------------------------------ */
+
+using EventProcess;
 using UIFramework;
+using UIFramework.Panel;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,7 +31,13 @@ namespace SceneUI.StartSceneUI
         void OnStartButtonPressDown()
         {
             UIFrame.HideUI("StartMainPanel");
+
             Debug.Log("StartMainPanelHide");
+        }
+
+        public void TestButton()
+        {
+            UIFrame.ShowUI("StartMainPanel",new StartMainPanelProperties(PanelPriority.None, "启动测试"));
         }
     }
 }
